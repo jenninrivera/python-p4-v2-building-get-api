96d5446ce59e787b9f065dd4f4ad1c0d26fd3dba
Jenni Rivera <jenninrivera@gmail.com>
2023-10-16 20:11:05 -0400

    completed code along

--- server/models.py
+++ server/models.py
@@ -14,9 +14,9 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 
 
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = "games"
-
+    serialize_rules = ('-reviews.game',)
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
     genre = db.Column(db.String)
@@ -31,9 +31,9 @@ class Game(db.Model):
         return f"<Game {self.title} for {self.platform}>"
 
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = "reviews"
-
+    serialize_rules = ('-user.reviews','-game.reiews',)
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
@@ -46,13 +46,15 @@ class Review(db.Model):
     game = db.relationship("Game", back_populates="reviews")
     user = db.relationship("User", back_populates="reviews")
 
+    users = association_proxy("reviews", "user", creator=lambda user_obj: Review(user=user_obj))
+
     def __repr__(self):
         return f"<Review ({self.id}) of {self.game}: {self.score}/10>"
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = "users"
-
+    serialize_rules = ('-reviews.user',)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
